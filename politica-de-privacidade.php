3c0481d42e124b2883d49e9122b70e7ec2441296
jeanprodutivo <45724694+jeanprodutivo@users.noreply.github.com>
2022-04-04 17:51:07 -0300

    Add files via upload

---
+++ politica-de-privacidade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title> Cavalo Grande | Site OFICIAL </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="code" content="Lucas Grego - [ Contato → https://linktr.ee/lucas_grego ]">
+
+<meta name="description" content=" Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande.">
+<meta name="author" content="Lucas Grego">
+<meta name="keywords" content="aumento peniano, cavalo grande" />
+<link rel="alternate" href="" hreflang="pt-br" />
+<meta name="robots" content="index, follow">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta property="og:url" content="https://cavalogrande.com//" />
+<meta property="og:title" content="Cavalo Grande | Site OFICIAL" />
+<meta property="og:image" content="https://cavalogrande.com//midia/img/og-img.jpg" />
+<meta property="og:description" content="Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande." />
+<meta name="theme-color" content="#fe2813">
+<meta property="business:contact_data:country_name" content="Brasil" />
+<meta property="business:contact_data:website" content="https://cavalogrande.com//" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:description" content="Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande." />
+<meta name="twitter:title" content="Cavalo Grande | Site OFICIAL" />
+<meta name="twitter:image" content="https://cavalogrande.com//midia/img/og-img.jpg" />
+<meta name="geo.region" content="BR" />
+<meta name="description" content="Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande." />
+<link rel="canonical" href="https://cavalogrande.com//" />
+<meta property="og:type" content="website" />
+<meta property="og:locale" content="pt_BR" />
+<meta name="format-detection" content="telephone=no">
+
+
+<link rel="stylesheet" href="libs/css/bootstrap.min.css">
+<link rel="stylesheet" href="libs/css/reseat.css">
+<link rel="stylesheet" type="text/css" href="libs/css/css.css">
+<link rel="stylesheet" type="text/css" href="libs/css/aos.css">
+<link rel="stylesheet" type="text/css" href="libs/css/floating-wpp.css">
+<meta name="theme-color" content="#fafafa">
+<link rel="shortcut icon" href="midia/img/x-icon.png" type="image/x-icon">
+<link rel="icon" href="midia/img/x-icon.png" type="image/x-icon">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+
+
+
+</head>
+
+<meta name="theme-color" content="#fafafa">
+<style type="text/css">
+  	section{
+  		padding: 20px;
+  	}
+  	p{
+  		font-weight: 300;
+  		font-size: 17px;
+  		line-height: 1.6em;
+  	}
+  	h1,h2{
+  		font-size: 30px;
+  		font-style: normal;
+  		color: #FFF;
+  	}
+  	.sub-conteudo h2{
+  		font-size: 25px;
+  		color: #343a40;
+  	}
+  </style>
+<body>
+
+<section class="bg-dark">
+<div class="container">
+<div class="row">
+<div class="col-12 text-center">
+<h2 class="m-0 text-uppercase"> Cavalo Grande </h2>
+</div>
+</div>
+</div>
+</section>
+
+<section>
+<div class="container sub-conteudo">
+<h2>Política de privacidade para <strong>Cavalo Grande</strong></h2>
+<p>Todas as suas informações pessoais recolhidas, serão usadas para o ajudar a tornar a sua visita no nosso site o mais produtiva e agradável possível.
+</p>
+<p>A garantia da confidencialidade dos dados pessoais dos utilizadores do nosso site é importante para o Cavalo Grande.
+</p>
+<p>
+Todas as informações pessoais relativas a membros, assinantes, clientes ou visitantes que usem o Cavalo Grande serão tratadas em concordância com a Lei da Proteção de Dados Pessoais de 26 de outubro de 1998 (Lei n.º 67/98).
+</p>
+<p>
+A informação pessoal recolhida pode incluir o seu nome, e-mail, número de telefone e/ou telemóvel, morada, data de nascimento e/ou outros.
+</p>
+<p>
+O uso do Cavalo Grande pressupõe a aceitação deste Acordo de privacidade. A equipa do Cavalo Grande reserva-se ao direito de alterar este acordo sem aviso prévio. Deste modo, recomendamos que consulte a nossa política de privacidade com regularidade de forma a estar sempre atualizado.
+</p>
+<h2>Os anúncios</h2>
+<p>
+Tal como outros websites, coletamos e utilizamos informação contida nos anúncios. A informação contida nos anúncios, inclui o seu endereço IP (Internet Protocol), o seu ISP (Internet Service Provider, como o Sapo, Clix, ou outro), o browser que utilizou ao visitar o nosso website (como o Internet Explorer ou o Firefox), o tempo da sua visita e que páginas visitou dentro do nosso website.
+</p>
+<h2>Os Cookies e Web Beacons</h2>
+<p>
+tilizamos cookies para armazenar informação, tais como as suas preferências pessoas quando visita o nosso website. Isto poderá incluir um simples popup, ou uma ligação em vários serviços que providenciamos, tais como fóruns.
+</p>
+<p>
+Em adição também utilizamos publicidade de terceiros no nosso website para suportar os custos de manutenção. Alguns destes publicitários, poderão utilizar tecnologias como os cookies e/ou web beacons quando publicitam no nosso website, o que fará com que esses publicitários (como o Google através do Google AdSense) também recebam a sua informação pessoal, como o endereço IP, o seu ISP, o seu browser, etc. Esta função é geralmente utilizada para geotargeting (mostrar publicidade de Lisboa apenas aos leitores oriundos de Lisboa por ex.) ou apresentar publicidade direcionada a um tipo de utilizador (como mostrar publicidade de restaurante a um utilizador que visita sites de culinária regularmente, por ex.).
+</p>
+<p>
+Você detém o poder de desligar os seus cookies, nas opções do seu browser, ou efetuando alterações nas ferramentas de programas Anti-Virus, como o Norton Internet Security. No entanto, isso poderá alterar a forma como interage com o nosso website, ou outros websites. Isso poderá afetar ou não permitir que faça logins em programas, sites ou fóruns da nossa e de outras redes.</p>
+ <h2>Ligações a Sites de terceiros</h2>
+<p>
+O Cavalo Grande possui ligações para outros sites, os quais, a nosso ver, podem conter informações / ferramentas úteis para os nossos visitantes. A nossa política de privacidade não é aplicada a sites de terceiros, pelo que, caso visite outro site a partir do nosso deverá ler a politica de privacidade do mesmo.
+</p>
+<p>Não nos responsabilizamos pela política de privacidade ou conteúdo presente nesses mesmos sites.</p>
+</div>
+</section>
+
+<section class="bg-dark">
+<div class="container">
+<div class="row">
+<div class="col-12 text-center">
+<h2 class="m-0 text-uppercase"> Cavalo Grande </h2>
+</div>
+</div>
+</div>
+</section>
+</body>
+</html>
---
+++ termos-de-uso.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title> Cavalo Grande | Site OFICIAL </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="code" content="Lucas Grego - [ Contato → https://linktr.ee/lucas_grego ]">
+
+<meta name="description" content=" Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande.">
+<meta name="author" content="Lucas Grego">
+<meta name="keywords" content="aumento peniano, cavalo grande" />
+<link rel="alternate" href="" hreflang="pt-br" />
+<meta name="robots" content="index, follow">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta property="og:url" content="https://cavalogrande.com//" />
+<meta property="og:title" content="Cavalo Grande | Site OFICIAL" />
+<meta property="og:image" content="https://cavalogrande.com//midia/img/og-img.jpg" />
+<meta property="og:description" content="Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande." />
+<meta name="theme-color" content="#fe2813">
+<meta property="business:contact_data:country_name" content="Brasil" />
+<meta property="business:contact_data:website" content="https://cavalogrande.com//" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:description" content="Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande." />
+<meta name="twitter:title" content="Cavalo Grande | Site OFICIAL" />
+<meta name="twitter:image" content="https://cavalogrande.com//midia/img/og-img.jpg" />
+<meta name="geo.region" content="BR" />
+<meta name="description" content="Turbine suas ereções, ganhe mais centimentros para o seu amigão, satisfaça qualquer mulher na cama com Cavalo Grande." />
+<link rel="canonical" href="https://cavalogrande.com//" />
+<meta property="og:type" content="website" />
+<meta property="og:locale" content="pt_BR" />
+<meta name="format-detection" content="telephone=no">
+
+
+<link rel="stylesheet" href="libs/css/bootstrap.min.css">
+<link rel="stylesheet" href="libs/css/reseat.css">
+<link rel="stylesheet" type="text/css" href="libs/css/css.css">
+<link rel="stylesheet" type="text/css" href="libs/css/aos.css">
+<link rel="stylesheet" type="text/css" href="libs/css/floating-wpp.css">
+<meta name="theme-color" content="#fafafa">
+<link rel="shortcut icon" href="midia/img/x-icon.png" type="image/x-icon">
+<link rel="icon" href="midia/img/x-icon.png" type="image/x-icon">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+
+
+
+</head>
+
+<meta name="theme-color" content="#fafafa">
+<style type="text/css">
+  	section{
+  		padding: 20px;
+  	}
+  	p{
+  		font-weight: 300;
+  		font-size: 17px;
+  		line-height: 1.6em;
+  	}
+  	h1,h2{
+  		font-size: 30px;
+  		font-style: normal;
+  		color: #FFF;
+  	}
+  	.sub-conteudo h2{
+  		font-size: 25px;
+  		color: #343a40;
+  	}
+  </style>
+<body>
+
+<section class="bg-dark">
+<div class="container">
+<div class="row">
+<div class="col-12 text-center">
+<h2 class="m-0 text-uppercase"> Cavalo Grande </h2>
+</div>
+</div>
+</div>
+</section>
+
+<section class="my-5 sub-conteudo">
+<div class="container">
+<h2>Termos e Condições de Compra e Venda de Cavalo Grande</h2>
+<p>Considerando que a Cavalo Grande realiza venda de produtos e serviços pela internet; Considerando o interesse do Cliente na compra dos produtos oferecidos pela Cavalo Grande (“Produtos”) em seus canais de venda; O presente contrato tem por finalidade estabelecer as condições gerais de uso e compra de produtos e serviços do cliente do site Cavalo Grande.
+</p>
+<p>I. Confidencialidade: é de responsabilidade da Cavalo Grande a preservação da confidencialidade de todos os dados e informações fornecidos pelo Cliente no processo de compra. A segurança do site é auditada diariamente e garantida contra a ação de hackers, através do selo “Site Blindado”.
+</p>
+<p>II. Serviço de Atendimento ao Cliente (SAC): O cliente dispõe desse serviço para sanar suas dúvidas, solucionar eventuais solicitações ou reclamações a respeito do seu pedido ou de qualquer conteúdo disponibilizado no site. O SAC poderá ser acionado por meio de telefone ou de formulário do site.
+</p>
+<p>III. Política de entrega: o prazo para entrega dos Produtos é informado durante o procedimento de compra, contabilizado em dias úteis. As entregas dos Produtos são realizadas de segunda a sexta-feira, das 8h às 22h. Excepcionalmente, algumas entregas de Produtos podem ocorrer aos sábados, domingos e feriados.
+</p>
+<p>III.I – A conferência da adequação das dimensões do produto é de responsabilidade do Cliente, que deverá se assegurar de que estas estão de acordo com os limites espaciais dos elevadores, portas e corredores do local da entrega. Não será realizada a montagem ou desmontagem do produto, transporte pela escada e/ou portas e janelas, ou içamento das entregas.
+</p>
+<p>III.II – Serão realizadas até três tentativas de entrega no local informado, em dias alternados, com intervalo de até 48h entre uma entrega e outra. É indispensável que, no endereço solicitado, haja uma pessoa autorizada pelo comprador, maior de 18 anos, e portando documento de identificação para receber a mercadoria e assinar o protocolo de entrega. Se houver três tentativas de entrega sem sucesso, o pedido retornará para o Centro de Distribuição da Cavalo Grande.
+</p>
+<p>III.III – Após a finalização do pedido não é possível alterar a forma de pagamento e/ou endereço de entrega, solicitar adiantamento ou, ainda, prioridade da entrega.
+</p>
+<p>III.IV – O prazo de entrega informado durante o procedimento de compra do Produto leva em consideração o estoque, a região, o processo de emissão da nota fiscal e o tempo de preparo do produto. A cada atualização no status de entrega do pedido, o sistema da Cavalo Grande envia, automaticamente, e-mails de alerta para o Cliente.
+</p>
+<p>III.V – O valor do frete da entrega é calculado com base no local de entrega, peso e dimensões do Produto.
+</p>
+<p>III.VI – A Cavalo Grande não autoriza a transportadora a: entrar no domicílio; entregar por meios alternativos (exemplo: içar produto por janela); realizar instalação ou manutenção de produtos; abrir a embalagem do produto; realizar entrega em endereço diferente do que consta no DANFE; realizar entrega a menor de idade ou sem documento de identificação.
+</p>
+<p>III.VII – A Cavalo Grande não se responsabiliza pela retenção de mercadorias na SEFAZ quando esta se dever exclusivamente a pendências do cliente, sendo, portanto, necessário seu comparecimento no posto fiscal para que a mercadoria seja liberada, tendo em vista que nestes casos as informações referentes a liberações e pagamentos só são passadas aos interessados.
+</p>
+<p>IV. Direito de arrependimento: ao Cliente será facultado o exercício do direito de arrependimento da compra, com a finalidade de devolução do Produto, hipótese na qual deverão ser observadas as seguintes condições: o prazo de desistência da compra do produto é de até 7 (sete) dias corridos, a contar da data do recebimento; em caso de devolução, o produto deverá ser devolvido à Cavalo Grande na embalagem original, acompanhado do DANFE (Documento Auxiliar da Nota Fiscal Eletrônica), do manual e de todos os seus acessórios.
+</p>
+<p>IV.I – O Cliente deverá solicitar a devolução através do Serviço de Atendimento ao Cliente (SAC) ou diretamente no Painel de Controle, no tópico “cancelar pedido”. As despesas decorrentes de coleta ou postagem do Produto serão custeadas pela Cavalo Grande.
+</p>
+<p>IV.II – Após a chegada do produto ao Centro de Distribuição, a Cavalo Grande verificará se as condições supra citadas foram atendidas. Em caso afirmativo, providenciará a restituição no valor total da compra.
+</p>
+<p>IV.III – Em compras com cartão de crédito a administradora do cartão será notificada e o estorno ocorrerá na fatura seguinte ou na posterior, de uma só vez, seja qual for o número de parcelas utilizado na compra. O prazo de ressarcimento e, ainda, a cobrança das parcelas remanescentes após o estorno integral do valor do Produto no cartão de crédito do Cliente realizado pela Cavalo Grande, é de responsabilidade da administradora do cartão. Na hipótese de cobrança de parcelas futuras pela administradora do cartão, o Cliente não será onerado, vez que a Cavalo Grande, conforme mencionado acima, realiza o estorno do valor integral do Produto em uma única vez, sendo o crédito referente ao estorno concedido integralmente pela administradora do cartão na fatura de cobrança subsequente ao mês do cancelamento.
+</p>
+<p>IV.IV – Em compras pagas com boleto bancário ou débito em conta, a restituição será efetuada por meio de depósito bancário, em até 10 (dez) dias úteis, somente na conta corrente do(a) comprador(a), que deve ser individual. É necessário que o CPF do titular da conta corrente.
+</p>
+</div>
+</section>
+
+<section class="bg-dark">
+<div class="container">
+<div class="row">
+<div class="col-12 text-center">
+<h2 class="m-0 text-uppercase"> Cavalo Grande </h2>
+</div>
+</div>
+</div>
+</section>
+</body>
+</html>
